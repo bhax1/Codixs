acd9f351085921ff96891d711694ebd6684f2e4b
Mixscoo2 <160635796+Mixscoo2@users.noreply.github.com>
2024-02-29 23:53:40 +0800

    FOOTER - V2
    Wave Pool

--- index.php
+++ index.php
@@ -27,8 +27,18 @@
       </ul>
 
       <div class="main">
-        <a href="#" class="action_btn_login"><span>Log In</span></a>
+        <form>
+          <a href="#" class="action_btn_login" onclick="redirectToLogin()"><span>Log In</span></a>
+        </form>
+
+        <script>
+          function redirectToLogin() {
+            window.location.href = 'login.php';
+          }
+        </script>
+
         <div class="fa-solid fa-bars" id="menu_icon"></div>
+        </form>
       </div>
     </nav>
   </header>
@@ -39,9 +49,7 @@
     </div>
     <div class="hero">
       <h1><span class="auto-type"></span></h1>
-      <form>
-        <input type="submit" class="Getting_btn" value="Getting Started"></input>
-      </form>
+      <input type="submit" class="Getting_btn" value="Getting Started"></input>
     </div>
   </section>
 
@@ -49,39 +57,36 @@
     <div class="next">
     </div>
   </section>
+
   <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
   <script type="text/javascript" src="script.js"></script>
-  <script src="https://cdn.tailwindcss.com"></script>
 </body>
-  <footer class="footer">
-    <div class="waves">
-      <div class="wave" id="wave1"></div>
-      <div class="wave" id="wave2"></div>
-      <div class="wave" id="wave3"></div>
-      <div class="wave" id="wave4"></div>
-    </div>
-    <ul class="social-icon">
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-facebook"></ion-icon>
-        </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-twitter"></ion-icon>
-        </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-linkedin"></ion-icon>
-        </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-instagram"></ion-icon>
-        </a></li>
-    </ul>
-    <ul class="menu">
-      <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">About Us</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Blog</a></li>
-    </ul>
-    <p>&copy;2024 CodixGo | All Rights Reserved</p>
-  </footer>
-  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<!-- FOOTER -->
+<footer class="footer">
+  <div class="waves">
+    <div class="wave" id="wave1"></div>
+    <div class="wave" id="wave2"></div>
+    <div class="wave" id="wave3"></div>
+    <div class="wave" id="wave4"></div>
+  </div>
+  <ul class="social-icon">
+    <li class="social-icon__item"><a class="social-icon__link" href="#">
+        <ion-icon name="logo-facebook"></ion-icon>
+      </a></li>
+    <li class="social-icon__item"><a class="social-icon__link" href="#">
+        <ion-icon name="logo-twitter"></ion-icon>
+      </a></li>
+    <li class="social-icon__item"><a class="social-icon__link" href="#">
+        <ion-icon name="logo-linkedin"></ion-icon>
+      </a></li>
+    <li class="social-icon__item"><a class="social-icon__link" href="#">
+        <ion-icon name="logo-instagram"></ion-icon>
+      </a></li>
+  </ul>
+  <p>&copy;2024 CodixGo | All Rights Reserved</p>
+</footer>
+<!-- SCRIPT FOR FOOTER -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 </html>
