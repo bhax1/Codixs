847f922cd37fa42b752efd51d1697406fe31ba69
Mixscoo2 <160635796+Mixscoo2@users.noreply.github.com>
2024-02-29 23:43:14 +0800

    FOOTER
    Wave Pool

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,11 +9,10 @@
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="hero.css">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-  crossorigin="anonymous" referrerpolicy="no-referrer"/>
-  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
+
 <body>
 
   <header>
@@ -25,29 +25,23 @@
         <li><a href="#">Services</a></li>
         <li><a href="#">Blog</a></li>
       </ul>
-      
-      <div class="main">
-      <form>
-  <a href="#" class="action_btn_login" onclick="redirectToLogin()"><span>Log In</span></a>
-</form>
 
-<script>
-function redirectToLogin() {
-  window.location.href = 'login.php';
-}
-</script>
-    
+      <div class="main">
+        <a href="#" class="action_btn_login"><span>Log In</span></a>
         <div class="fa-solid fa-bars" id="menu_icon"></div>
-        </form>
-    </div>
+      </div>
     </nav>
   </header>
 
   <section class="page1">
-    <div class="gradient-background"><div class="gradient"></div></div>
+    <div class="gradient-background">
+      <div class="gradient"></div>
+    </div>
     <div class="hero">
       <h1><span class="auto-type"></span></h1>
-        <input type="submit" class="Getting_btn" value="Getting Started" ></input>
+      <form>
+        <input type="submit" class="Getting_btn" value="Getting Started"></input>
+      </form>
     </div>
   </section>
 
@@ -55,9 +49,39 @@ function redirectToLogin() {
     <div class="next">
     </div>
   </section>
-  
   <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
   <script type="text/javascript" src="script.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
 </body>
+  <footer class="footer">
+    <div class="waves">
+      <div class="wave" id="wave1"></div>
+      <div class="wave" id="wave2"></div>
+      <div class="wave" id="wave3"></div>
+      <div class="wave" id="wave4"></div>
+    </div>
+    <ul class="social-icon">
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul class="menu">
+      <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">About Us</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Blog</a></li>
+    </ul>
+    <p>&copy;2024 CodixGo | All Rights Reserved</p>
+  </footer>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </html>
-
